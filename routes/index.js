862595fa5631eb38ad5e154886e1fac59bd33c1c
samsamsue <yiroon@qq.com>
2024-10-29 11:21:13 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.send('Hello World!<img src="/getThumb">');
+    res.send('Hello World!');
 });
 
 // 定义路由
